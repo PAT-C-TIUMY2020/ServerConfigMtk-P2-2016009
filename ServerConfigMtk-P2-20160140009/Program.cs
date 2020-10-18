8dc6584cf162374bead007628dc5992002538f39
Arrasy <arrasyjunior@gmail.com>
2020-10-19 03:56:40 +0700

    Add project files.

---
+++ ServerConfigMtk-P2-20160140009/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using ServiceMtk_P2_20160140009;
+
+namespace ServerConfigMtk_P2_20160140009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
